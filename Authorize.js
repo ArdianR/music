a73a65cec0beb4890f269f52ffb6b32453dfc6aa
ArdianR <fianr5750@gmail.com>
2019-01-28 17:25:27 +0700

    code asli

--- Authorize.js
+++ Authorize.js
@@ -19,12 +19,17 @@ var client_id = 'a4fd5d5325454189ba2c3dec27f6fa8c';
 var client_secret = '9c3e9e1ee9394015af9f5db874ccfca2';
 var redirect_uri = 'music://spotify.development';
 
-export default class Authorize extends Component {
+class Authorize extends Component {
     constructor(props) {
         super(props);
         this.state = {
             url: `https://accounts.spotify.com/authorize?client_id=` + client_id + `&response_type=code&show_dialog=true&redirect_uri=` + redirect_uri + `&scope=user-read-private user-read-email&state=34fFs29kd09`,
+            code: '',
             auth: base64.encode(client_id+`:`+client_secret),
+            access_token: '',
+            refresh_token: '',
+            display_name: '',
+            id: ''
         };
     }
 
@@ -73,13 +78,38 @@ export default class Authorize extends Component {
   render() {
     return (
       <View>
-        <Text>code = {this.state.code}</Text>
-        <Text>access_token = {this.state.access_token}</Text>
-        <Text>refresh_token = {this.state.refresh_token}</Text>
-        <Text>display_name = {this.state.display_name}</Text>
-        <Text>id = {this.state.id}</Text>
+        <Text>{this.state.display_name}</Text>
       </View>
     );
   }
 
 }
+
+class Token extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            url: this.props.navigation.getParam('url'),
+        };
+    }
+
+  render() {
+    return (
+      <View>
+        <Text>{this.state.url}</Text>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator(
+  {
+      Authorize: Authorize,
+      Token: Token,
+  },
+  {
+      initialRouteName: 'Authorize',
+  }
+)
+
+export default createAppContainer(AppNavigator)
