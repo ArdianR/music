fc813896f76ee710686fafcfa588cde9d692bdb4
ArdianR <fianr5750@gmail.com>
2019-01-31 10:23:59 +0700

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,252 @@
+{
+    "categories": {
+        "href": "https://api.spotify.com/v1/browse/categories?offset=0&limit=20",
+        "items": [
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/toplists",
+                "icons": [
+                    {
+                        "height": 275,
+                        "url": "https://t.scdn.co/media/derived/toplists_11160599e6a04ac5d6f2757f5511778f_0_0_275_275.jpg",
+                        "width": 275
+                    }
+                ],
+                "id": "toplists",
+                "name": "Top Lists"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/popindo",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/categories/popindo_274x274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "popindo",
+                "name": "Pop Indo"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/pop",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/derived/pop-274x274_447148649685019f5e2a03a39e78ba52_0_0_274_274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "pop",
+                "name": "Pop"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/mood",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/original/mood-274x274_976986a31ac8c49794cbdc7246fd5ad7_274x274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "mood",
+                "name": "Mood"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/chill",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/derived/chill-274x274_4c46374f007813dd10b37e8d8fd35b4b_0_0_274_274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "chill",
+                "name": "Chill"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/kpop",
+                "icons": [
+                    {
+                        "height": null,
+                        "url": "https://t.scdn.co/images/69c728f3bd9643a5ab0f4ef5a79919f1.jpeg",
+                        "width": null
+                    }
+                ],
+                "id": "kpop",
+                "name": "K-Pop"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/decades",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/derived/decades_9ad8e458242b2ac8b184e79ef336c455_0_0_274_274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "decades",
+                "name": "Decades"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/romance",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/derived/romance-274x274_8100794c94847b6d27858bed6fa4d91b_0_0_274_274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "romance",
+                "name": "Romance"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/travel",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/derived/travel-274x274_1e89cd5b42cf8bd2ff8fc4fb26f2e955_0_0_274_274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "travel",
+                "name": "Travel"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/focus",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/original/genre-images-square-274x274_5e50d72b846a198fcd2ca9b3aef5f0c8_274x274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "focus",
+                "name": "Focus"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/sleep",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/derived/sleep-274x274_0d4f836af8fab7bf31526968073e671c_0_0_274_274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "sleep",
+                "name": "Sleep"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/rock",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/derived/rock_9ce79e0a4ef901bbd10494f5b855d3cc_0_0_274_274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "rock",
+                "name": "Rock"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/edm_dance",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/derived/edm-274x274_0ef612604200a9c14995432994455a6d_0_0_274_274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "edm_dance",
+                "name": "Electronic/Dance"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/hiphop",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/original/hip-274_0a661854d61e29eace5fe63f73495e68_274x274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "hiphop",
+                "name": "Hip-Hop"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/workout",
+                "icons": [
+                    {
+                        "height": null,
+                        "url": "https://t.scdn.co/media/links/workout-274x274.jpg",
+                        "width": null
+                    }
+                ],
+                "id": "workout",
+                "name": "Workout"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/jazz",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/derived/jazz-274x274_d6f407453a1f43d3163c55cca624a764_0_0_274_274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "jazz",
+                "name": "Jazz"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/reggae",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/derived/reggae-274x274_2f11a0500528532b3bc580e3428e9610_0_0_274_274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "reggae",
+                "name": "Reggae"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/party",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/links/partyicon_274x274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "party",
+                "name": "Party"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/karaoke",
+                "icons": [
+                    {
+                        "height": 274,
+                        "url": "https://t.scdn.co/media/categories/karaoke_274x274.jpg",
+                        "width": 274
+                    }
+                ],
+                "id": "karaoke",
+                "name": "Karaoke"
+            },
+            {
+                "href": "https://api.spotify.com/v1/browse/categories/family",
+                "icons": [
+                    {
+                        "height": null,
+                        "url": "https://t.scdn.co/images/ab1e1dbf-1ea2-403e-8c11-5dba87c9c720.jpg",
+                        "width": null
+                    }
+                ],
+                "id": "family",
+                "name": "Kids & Family"
+            }
+        ],
+        "limit": 20,
+        "next": "https://api.spotify.com/v1/browse/categories?offset=20&limit=20",
+        "offset": 0,
+        "previous": null,
+        "total": 42
+    }
+}
